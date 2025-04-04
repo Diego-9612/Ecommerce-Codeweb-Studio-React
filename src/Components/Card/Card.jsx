e2ead36fe4239a1e080ecef1d57c471cc8ba59b8
Diego Fernando Guerrero Perez <diego.guerrero9612@gmail.com>
2025-04-03 20:40:51 -0500

    Realizamos la conexion a la Api y consumimos el endpoint de productos

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,15 +1,15 @@
-import Img from '../../../public/pexels-jessbaileydesign-788946.jpg'
-const Card = () => {
+
+const Card = (data) => {
     return(
         <section className="w-56 bg-white rounded-lg cursor-pointer h-60">
             <figure className="relative w-full mb-2 h-4/5">
-                <span className="absolute bottom-0 left-0 m-2 text-xs text-black rounded-lg bg-white/60 px-3 py-0.5">Electrodomesticos</span>
-                <img src={Img} alt="Iphone" className='object-cover w-full h-full rounded-lg' />
+                <span className="absolute bottom-0 left-0 m-2 text-xs text-black rounded-lg bg-white/60 px-3 py-0.5">{data.data.category.name}</span>
+                <img src={data.data.images[0]} alt={data.data.title} className='object-cover w-full h-full rounded-lg' />
                 <div className="absolute top-0 right-0 flex items-center justify-center w-6 h-6 p-1 m-2 bg-white border rounded-full">+</div>
             </figure>
             <p className='flex items-center justify-between'>
-                <span className='text-sm font-light'>Iphone</span>
-                <span className='text-lg font-medium'>$300</span>
+                <span className='text-sm font-light'>{data.data.title}</span>
+                <span className='text-lg font-medium'>${data.data.price}</span>
             </p>
         </section>
     );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,32 @@
+import { useState, useEffect } from "react";
 import { Layout } from "../../Components/Layout/Layout";
 import { Card } from "../../Components/Card/Card";
 function Home() {
+
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products').
+            then(response => response.json()).
+            then(data => setItems(data));
+    }, [])
 
     return (
 
         <Layout>
             Home
-            <Card/>
+            <main className="grid w-full max-w-screen-lg grid-cols-4 gap-4">
+                {
+                    items?.map(data => (
+                        <Card key={data.id} data={data} />
+                    ))
+                }
+            </main>
         </Layout>
-            
 
-        
+
+
     )
 }
 
-export  {Home};
+export { Home };
